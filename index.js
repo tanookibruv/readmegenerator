9b562bac845633aa704b915ef3031ae1529891e0
tanookibruv <erendon1836@gmail.com>
2021-09-27 00:10:06 -0700

    attempting to use promisify

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-const inquirer = require('inquirer');
+const prompt = require('inquirer');
+const fs = require('fs');
+const promisify = require('util');
+const writeFile = promisify(fs);
 
-inquirer
-    .prompt([
+
+const questions = [
         {
             type: 'input',
-            message: 'Name your README file.',
-            name: 'name',
+            message: 'What is your GitHub username?',
+            name: 'github',
+
+        },
+        {
+            type: 'input',
+            message: 'What is the name of your project?',
+            name: 'title',
         },
         {
             type: 'input',
             message: 'Describe your project.',
             name: 'description',
         },
+        {
+            type: 'input',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            name: 'license',
+        }
+],
 
-    ])
+function promptQuestions() {
+    return prompt(questions);
+};
+
+async function mainFunc() {
+    try {
+        const answers = await promptQuestions();
+
+        const readme = generateREADME(answers);
+
+        await writeFile('README.md', readme);
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+mainFunc();
