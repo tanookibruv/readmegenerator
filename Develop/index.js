d97367d74bfc25e961a60a9fab94b06d6c94e079
tanookibruv <erendon1836@gmail.com>
2021-10-03 19:07:48 -0700

    updating with proper questions and running function

--- Develop/index.js
+++ Develop/index.js
@@ -2,13 +2,59 @@
 const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username.',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description about your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependecies?',
+        default: 'npm i',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What do future users need to know about the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What do future users need to know about contributing to the repo?',
+        name: 'contributing',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
